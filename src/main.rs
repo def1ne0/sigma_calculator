3425293653e46d88794047ee4b1496852c6daccd
Arseniy Finevich <clumserinri537@gmail.com>
2025-12-29 23:09:29 +0300

    добавлен норм выход и уважение к виндовс юзерам

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use iced::{Alignment, Element, Fill};
+use iced::{Alignment, Element, Fill, Task};
 use iced::widget::{button, column, container, text, text_input};
 use meval::eval_str;
-use winapi::shared::ntdef::{FALSE, TRUE};
-use winapi::um::winuser::{ExitWindowsEx, EWX_POWEROFF, EWX_FORCE};
-use winapi::um::powrprof::SetSuspendState;
 
 pub fn main() -> iced::Result {
     iced::run(Calculator::update, Calculator::view)
@@ -31,26 +28,20 @@ impl Calculator {
         }
     }
 
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, message: Message) -> Task<Message>{
         match message {
             Message::InputChanged(value) => {
                 self.expression = value;
+                Task::none()
             },
             Message::Evaluate => {
                 self.result = eval_str(self.expression.clone())
                     .unwrap_or(f64::NAN)
-                    .to_string()
+                    .to_string();
+                Task::none()
             }, 
             Message::Quit => {
-                let _ = iced::exit::<Calculator>();
-                
-                #[cfg(windows)]
-                unsafe {
-                    let status = ExitWindowsEx(EWX_POWEROFF | EWX_FORCE, 0);
-                    if status == 0 {
-                        let _ = SetSuspendState(FALSE, TRUE, TRUE);
-                    }
-                }
+                iced::exit::<Message>()
             }
         }
     }
