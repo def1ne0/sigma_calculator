f20ba14e8989fe59ed2c8c73858f28ab74e10ae1
Arseniy Finevich <finevich03@gmail.com>
2025-12-29 16:00:39 +0300

    used meval::eval_str for evaluating expressions

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,40 @@
-use iced::{Element, Fill, Alignment};
-use iced::widget::{button, row, column, text, text_input, container};
-
-#[derive(Debug, Clone, Default)]
-struct Calc {
-    first_value: String,
-    second_value: String,
-    result_value: String,
+use iced::{Alignment, Element, Fill};
+use iced::widget::{button, column, container, text, text_input};
+use meval::eval_str;
+pub fn main() -> iced::Result {
+    iced::run(Calculator::update, Calculator::view)
 }
 
 #[derive(Debug, Clone)]
 enum Message {
-    Plus,
-    Minus,
-    GetFirstValue(String),
-    GetSecondValue(String),
+    InputChanged(String),
+    Evaluate,
 }
 
-impl Calc {
+#[derive(Debug, Clone, Default)]
+struct Calculator {
+    expression: String,
+    result: String,
+}
+
+impl Calculator {
+    #[allow(unused)]
+    fn new() -> Calculator {
+        Calculator {
+            expression: String::new(),
+            result: String::new(),
+        }
+    }
+
     fn update(&mut self, message: Message) {
         match message {
-            Message::Plus => {
-                let f_v = self.first_value.trim().parse::<f64>().unwrap();
-                let s_v = self.second_value.trim().parse::<f64>().unwrap();
-
-                self.result_value = (f_v + s_v).to_string();
+            Message::InputChanged(value) => {
+                self.expression = value;
             },
-            Message::Minus => {
-                let f_v = self.first_value.trim().parse::<f64>().unwrap();
-                let s_v = self.second_value.trim().parse::<f64>().unwrap();
-
-                self.result_value = (f_v - s_v).to_string();
-            },
-            Message::GetFirstValue(value) => {
-                self.first_value = value.clone();
-            },
-            Message::GetSecondValue(value) => {
-                self.second_value = value.clone();
+            Message::Evaluate => {
+                self.result = eval_str(self.expression.clone())
+                    .unwrap()
+                    .to_string()
             }
         }
     }
@@ -43,26 +42,16 @@ impl Calc {
     fn view(&self) -> Element<'_, Message> {
         container(
             column![
-                text_input("Введите первое число", &self.first_value)
-                    .on_input(Message::GetFirstValue)
-                    .padding(10),
-                row![
-                    button("+").on_press(Message::Plus),
-                    button("-").on_press(Message::Minus),
-                ],
-                text_input("Введите второе число", &self.second_value)
-                    .on_input(Message::GetSecondValue)
-                    .padding(10),
-                text(self.result_value.trim())
-                    .center(),
+                text_input("Input expression", &self.expression)
+                    .on_input(Message::InputChanged),
+                button("Evaluate")
+                    .on_press(Message::Evaluate),
+                text(self.result.trim()),
             ]
-                .align_x(Alignment::Center)
+                .align_x(Alignment::Center),
         )
             .center(Fill)
             .into()
     }
 }
 
-fn main() -> iced::Result {
-    iced::run(Calc::update, Calc::view)
-}
